68040edc4b9b6ff99804c9bf1a579d705b3c3ae6
DEN\megas <megassv10122003@gmail.com>
2025-03-22 17:54:17 +0200

    FixApp.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import Footer from "./components/footer/Footer.tsx";
 import HeaderComponent from "./components/header/HeaderComponent.tsx";
 import Main from "./components/main/Main.tsx";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   console.log("App");
   return (
     <div className="app">
-      <Router>
-        <HeaderComponent />
-        <Main />
-        <Footer />
-      </Router>
+      <HeaderComponent />
+      <Main />
+      <Footer />
     </div>
   );
 }
